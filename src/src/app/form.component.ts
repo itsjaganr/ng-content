be76630affbdc7d531c9b191c71b72bda9b4bc6e
itsjaganr <career.jagan@gmail.com>
2018-06-04 07:24:38 -0500

    Add files via upload

---
+++ src/src/app/form.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'form-component',
+  template: `
+  Hello..Form here Form here <ng-content></ng-content>`,
+})
+
+export class FormComponent { }
